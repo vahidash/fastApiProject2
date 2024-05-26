0f652674841e282b5ebf205a1bafb5a15d6b9028
vahidash <vahidash@yahoo.com>
2024-05-26 19:13:41 +0330

    test the GET request to read posts

--- tests/test_main.py
+++ tests/test_main.py
@@ -9,3 +9,7 @@ def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"msg": "Hello World"}
+
+
+def test_read_posts():
+    response = client.get("/posts")
